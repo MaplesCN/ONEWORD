d1db53935d62115209f0c253dd7c85061d9218b9
MaplesCN <chenyafengcn@foxmail.com>
2023-08-04 12:27:20 +0800

    uploads

---
+++ footer.php
@@ -0,0 +1,5 @@
+<?php $Now = date("Y-m-d");$Date = "2018-02-01";$d1 = strtotime($Now);$d = strtotime($Date);$Days = round(($d1-$d)/3600/24); ?><div id="footer"><footer id="colophon" class="site-footer"><?php get_template_part('template-parts/footer/footer','widgets'); ?><div class="site-info"><?php if(has_nav_menu('footer')):?><nav class="footer-navigation" aria-label="<?php esc_attr_e('Footer Menu','twentynineteen');?>"><?php wp_nav_menu(array('theme_location'=>'footer','menu_class'=>'footer-menu',));?></nav><?php endif; ?>
+<a id='dark-toggler' style="float:right"></a><li style="float:right"><a href="#top"><i class="fa-solid fa-caret-up"></i>顶部</a></li>
+<div style="margin-top:2rem" align="center"><div class="logo" style="height:30px;width:38.3px"></div>
+&copy<?php echo date('Y');?> <a href="<?php echo home_url(); ?>"><?php bloginfo('site_name');?></a><br><?php echo '第',$Days,'天';?></div>
+</div></footer></div><?php wp_footer(); ?></body></html>
