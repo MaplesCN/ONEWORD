21f0b4eda6c5a0c99960c3db384206f0846a629f
MaplesCN <chenyafengcn@foxmail.com>
2024-03-23 19:20:30 +0800

    ONEWORD

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div id="footer"><footer id="colophon" class="site-footer"><?php get_template_part('template-parts/footer/footer','widgets'); ?><div class="site-info"><?php if(has_nav_menu('footer')):?><nav class="footer-navigation" aria-label="<?php esc_attr_e('Footer Menu','twentynineteen');?>"><?php wp_nav_menu(array('theme_location'=>'footer','menu_class'=>'footer-menu',));?></nav><?php endif; ?>
-<a href="#top" style="float:right"><i class="fa-solid fa-caret-up"></i>顶部</a><li style="float:right"><a id='dark-toggler'></a></li>
-<div style="margin-top:2rem" align="center"><div class="logo" style="height:30px;width:38.3px"></div>
-&copy<?php echo date('Y');?> <a href="<?php echo home_url(); ?>"><?php bloginfo('site_name');?></a><br><?php echo '第',round((strtotime(date("Y-m-d"))-strtotime("2018-02-01"))/3600/24),'天';?></div>
+<a href="#top" style="float:right" title="顶部"><i class="fa-solid fa-caret-up"></i></a><li style="float:right"><a href="<?php echo home_url(); ?>" style="float:right" title="首页"><i class="fa-solid fa-igloo"></i></a></li><li style="float:right"><a id='dark-toggler' title="深/浅色"></a></li>
+<div style="margin-top:2rem" align="center"><div class="logo" style="height:30px;width:41px;margin-bottom:.2rem"></div>
+&copy <?php echo date('Y');?><br><?php echo '第',round((strtotime(date("Y-m-d"))-strtotime("2018-02-01"))/3600/24),'天';?></div>
 </div></footer></div><?php wp_footer(); ?></body></html>
--- header.php
+++ header.php
@@ -1,1 +1,27 @@
-<?php date_default_timezone_set("PRC");?><!doctype html><html <?php language_attributes();?>><head><meta charset="<?php bloginfo('charset');?>"/><meta content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0" name="viewport"><link rel="profile" href="https://gmpg.org/xfn/11"/><?php wp_head();?><link href="https://cdn.bootcdn.net/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet"><script src="https://cdn.staticfile.org/jquery/3.7.0/jquery.min.js"></script><script>var d='<i class="fa-solid fa-moon"></i>深色',m='<i class="fa-solid fa-lightbulb"></i>浅色',auto='<i class="fa-solid fa-circle-half-stroke"></i>自动',mode=localStorage.getItem("darkMode"),t="dark-toggler";$(function(){let e=document.getElementById("emoji").innerHTML;var o=e.match(/\p{Emoji_Presentation}/gu);for(o=Array.from(new Set(o)),i=0;i<o.length;i++)e=e.replaceAll(o[i],"<emoji>"+o[i]+"</emoji>");document.getElementById("emoji").innerHTML=e.replaceAll("</emoji><emoji>",""),"on"==mode?document.getElementById(t).innerHTML=m:"off"==mode?(document.getElementById(t).innerHTML=d,$("html").addClass("dark-mode")):(document.getElementById(t).innerHTML=auto,$("html").addClass("auto")),$("#dark-toggler").click(function(){mode=localStorage.getItem("darkMode"),$("html").addClass("toggler"),"on"==mode?($("html").toggleClass("dark-mode"),document.getElementById(t).innerHTML=d,localStorage.setItem("darkMode","off")):"off"==mode?($("html").toggleClass("dark-mode auto"),document.getElementById(t).innerHTML=auto,localStorage.setItem("darkMode","auto")):($("html").toggleClass("auto"),document.getElementById(t).innerHTML=m,localStorage.setItem("darkMode","on"))})})</script></head><body <?php body_class();?> id="emoji"><?php wp_body_open();?><div id="page pageContent" class="site"><div id="content" class="site-content"><?php if ( is_singular() && twentynineteen_can_show_post_thumbnail()):?><header id="masthead" class="<?php echo is_singular() && twentynineteen_can_show_post_thumbnail() ? 'site-header featured-image' : 'site-header'; ?>"><div class="site-featured-image"><?php twentynineteen_post_thumbnail(); the_post(); $discussion = ! is_page() && twentynineteen_can_show_post_thumbnail() ? twentynineteen_get_discussion_data() : null; $classes = 'entry-header'; if ( ! empty( $discussion ) && absint( $discussion->responses ) > 0 ) { $classes = 'entry-header has-discussion'; }?><div class="<?php echo $classes; ?>"><?php get_template_part( 'template-parts/header/entry', 'header' ); ?> </div><?php rewind_posts(); ?></div></header><?php endif; ?>
+<?php date_default_timezone_set("PRC");?>
+<!doctype html>
+<html <?php language_attributes();?> id="emoji">
+<head>
+	<meta charset="<?php bloginfo('charset');?>"/>
+	<meta content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0" name="viewport">
+	<link rel="profile" href="https://gmpg.org/xfn/11"/>
+	<?php wp_head();?>
+	<link href="https://cdn.bootcdn.net/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet">
+	<script src="https://cdn.staticfile.org/jquery/3.7.0/jquery.min.js"></script>
+	<script>var dark='<i class="fa-solid fa-moon"></i>',light='<i class="fa-solid fa-lightbulb"></i>',auto='<i class="fa-solid fa-circle-half-stroke"></i>',mode=localStorage.getItem("darkMode"),toggler="dark-toggler";$(function(){let content=document.getElementById("emoji").innerHTML, emoji=content.match(/\p{Emoji_Presentation}/gu);for(emoji=Array.from(new Set(emoji)),emo=0;emo<emoji.length;emo++){content=content.replaceAll(emoji[emo],"<emoji>"+emoji[emo]+"</emoji>");document.getElementById("emoji").innerHTML=content.replaceAll("</emoji><emoji>","")};"on"==mode?document.getElementById(toggler).innerHTML=light:"off"==mode?(document.getElementById(toggler).innerHTML=dark,$("html").addClass("dark-mode")):(document.getElementById(toggler).innerHTML=auto,$("html").addClass("auto")),$("#dark-toggler").click(function(){mode=localStorage.getItem("darkMode"),$("html").addClass("toggler"),"on"==mode?($("html").toggleClass("dark-mode"),document.getElementById(toggler).innerHTML=dark,localStorage.setItem("darkMode","off")):"off"==mode?($("html").toggleClass("dark-mode auto"),document.getElementById(toggler).innerHTML=auto,localStorage.setItem("darkMode","auto")):($("html").toggleClass("auto"),document.getElementById(toggler).innerHTML=light,localStorage.setItem("darkMode","on"))})})</script>
+</head>
+<body <?php body_class();?>>
+<?php wp_body_open();?>
+<div id="page" class="site">
+	<div id="content" class="site-content">
+		<?php if ( is_singular() && twentynineteen_can_show_post_thumbnail()):?>
+		<header id="masthead" class="<?php echo is_singular() && twentynineteen_can_show_post_thumbnail() ? 'site-header featured-image' : 'site-header'; ?>">
+			<div class="site-featured-image">
+				<?php twentynineteen_post_thumbnail(); the_post(); $discussion = ! is_page() && twentynineteen_can_show_post_thumbnail() ? twentynineteen_get_discussion_data() : null; $classes = 'entry-header'; if ( ! empty( $discussion ) && absint( $discussion->responses ) > 0 ) { $classes = 'entry-header has-discussion'; }?>
+				<div class="<?php echo $classes; ?>">
+					<?php get_template_part( 'template-parts/header/entry', 'header' ); ?>
+				</div>
+				<?php rewind_posts(); ?>
+			</div>
+		</header>
+		<?php endif; ?>
