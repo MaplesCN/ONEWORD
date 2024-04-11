16f61e32f4191eb21bf56727857584431dcfa47d
MaplesCN <chenyafengcn@foxmail.com>
2024-04-11 22:51:45 +0800

    update

--- footer.php
+++ footer.php
@@ -2,4 +2,4 @@
 <a href="#top" style="float:right" title="顶部"><i class="fa-solid fa-caret-up"></i></a><li style="float:right"><a href="<?php echo home_url(); ?>" style="float:right" title="首页"><i class="fa-solid fa-igloo"></i></a></li><li style="float:right"><a id='dark-toggler' title="深/浅色"></a></li>
 <div style="margin-top:2rem" align="center"><div class="logo" style="height:30.75px;width:42px;display:inline-block;vertical-align:middle;margin-bottom:.2rem"></div><div style="font-size:.8rem;font-weight:1000;color:#2f5596;user-select:none;display:inline-block;vertical-align:middle;margin-bottom:.2rem">·独白</div><br>
 &copy <?php echo date('Y');?><br><?php echo '第',round((strtotime(date("Y-m-d"))-strtotime("2018-02-01"))/3600/24),'天';?></div>
-</div></footer></div><?php wp_footer(); ?></body></html>
+</div></footer></div><?php wp_footer(); ?></div></div></body></html>
