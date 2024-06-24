845b471094b7c4b32cb10b0868f824cf62cd5e97
MaplesCN <chenyafengcn@foxmail.com>
2024-06-24 13:40:53 +0800

    updates

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div id="footer"><footer id="colophon" class="site-footer"><?php get_template_part('template-parts/footer/footer','widgets'); ?><div class="site-info"><?php if(has_nav_menu('footer')):?><nav class="footer-navigation" aria-label="<?php esc_attr_e('Footer Menu','twentynineteen');?>"><?php wp_nav_menu(array('theme_location'=>'footer','menu_class'=>'footer-menu',));?></nav><?php endif; ?>
 <a href="#top" style="float:right" title="顶部"><i class="fa-solid fa-caret-up"></i></a><li style="float:right"><a href="<?php echo home_url(); ?>" style="float:right" title="首页"><i class="fa-solid fa-igloo"></i></a></li><li style="float:right"><a id='dark-toggler' title="深/浅色"></a></li>
 <div style="margin-top:2rem" align="center"><div style="font-size:.8rem;font-weight:1000;color:#2f5596;user-select:none"><i class="oneword" style="font-size:1.2rem;font-weight:normal;vertical-align:middle"></i>·独白</div>
-&copy <?php echo date('Y');?><br><?php echo '第',round((strtotime(date("Y-m-d"))-strtotime("2018-02-01"))/3600/24),'天';?></div>
+<a href="/copyrights" target="_blank">&copy <?php echo date('Y');?></a><br><?php echo '第',round((strtotime(date("Y-m-d"))-strtotime("2018-02-01"))/3600/24),'天';?></div>
 </div></footer></div><?php wp_footer(); ?></div></div></body></html>
